980c50ba3b9c85584b87ccda654db2b98c44d80b
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-05-01 11:58:48 +0100

    Update Programming Exercises.py

--- Programming Exercises.py
+++ Programming Exercises.py
@@ -20,3 +20,50 @@ currentYear = 2018
 print(car['kms']/(currentYear - car['year']))
 
 #g
+#???
+
+#h
+print("")
+print("*H*")
+print("")
+car = dict({
+    
+    'make': "Audi",
+    'model': "A6",
+    'year' : 2013,
+    'kms' :52739,
+    'colour': "Silver",
+    'diesel': True,
+    'reg':"131 CN 6439",
+    })
+
+print(car['make'])
+print(car['model'])
+print(car['kms'])
+print(car['colour'][0])
+print(car['diesel'])
+print(car['reg'][4:5])
+currentYear = 2018
+print(car['kms'] / (currentYear - car['year']))
+
+#2
+print("")
+print("*2*")
+print("")
+#a
+print("a")
+colours = dict({
+    'white': "ban",
+    'red': "dearg",
+    'green': "gorm",
+    'blue': "glas",
+    'black': "dubh",
+    })
+colours['yellow'] = 'bui'
+print("")
+#c
+print("c")
+# Prompt the user to enter a colour
+colour = input("Enter a colour: ")
+# Look up the translation of colour
+translation = colours{colour}
